a00d73fbfff0d4f49d5044ad7f66a6f7520265aa
Mark Bates <markbates.339@gmail.com>
2022-09-22 17:14:43 +0100

    added simple terminal menu and dummy create method

--- src/main/kotlin/org/setu/wishlist/console/main/main.kt
+++ src/main/kotlin/org/setu/wishlist/console/main/main.kt
@@ -1,5 +1,56 @@
 package org.setu.wishlist.console.main
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>){
-    println("Hello Dave")
+    logger.info { "Launching Birthday Wishlist App" }
+
+    var input: Int
+    do {
+        input = menu()
+        when(input){
+            1 -> createWishlist()
+            -1 -> println("exiting, goodbye!")
+        }
+        println()
+    } while(input != -1)
+    logger.info { "Shutting Down" }
+
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Create New Wishlist")
+    println(" 2. Update Existing Wishlist")
+    println(" 3. Delete Wishlist")
+    println(" 4. Show all Wishlists")
+    println("-1. Exit")
+    println()
+    print("==> ")
+    input = readLine()!!
+    option = input.toInt()
+
+    return option
+}
+
+fun createWishlist(){
+    var title: String
+    var description: String
+    var attendees: String
+    var cost: Int
+
+    println("title: ")
+    title = readLine()!!
+    println("description: ")
+    description = readLine()!!
+    println("attendees: ")
+    attendees = readLine()!!
+    println("estimated cost: ")
+    cost = readLine()!!.toInt()
 }
