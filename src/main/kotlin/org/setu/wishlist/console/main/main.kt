e62e0959088e507d73df7d0abca923d5d306cb56
Mark Bates <markbates.339@gmail.com>
2022-09-28 11:09:47 +0100

    began implementing collections to the project

--- src/main/kotlin/org/setu/wishlist/console/main/main.kt
+++ src/main/kotlin/org/setu/wishlist/console/main/main.kt
@@ -4,7 +4,9 @@ import mu.KotlinLogging
 import org.setu.wishlist.console.models.WishlistModel
 
 private val logger = KotlinLogging.logger {}
-val wishlist = WishlistModel()
+
+val wishlists = ArrayList<WishlistModel>()
+
 
 
 fun main(args: Array<String>){
@@ -19,6 +21,7 @@ fun main(args: Array<String>){
             3 -> deleteWishlist()
             4 -> listWishlists()
             -1 -> println("exiting, goodbye!")
+            else -> println("invalid")
         }
         println()
     } while(input != -1)
@@ -48,6 +51,9 @@ fun menu() : Int {
 }
 
 fun createWishlist(){
+
+    val wishlist = WishlistModel()
+
     println("""===================================
         |=         CREATE WISHLIST         =
         |===================================""".trimMargin())
@@ -61,41 +67,57 @@ fun createWishlist(){
     println("estimated cost: ")
     wishlist.cost = readLine()!!
 
-    println("""===================================
+    if(wishlist.title.isNotEmpty() && wishlist.description.isNotEmpty() && wishlist.attendees.isNotEmpty() && wishlist.cost.isNotEmpty()){
+        wishlists.add(wishlist.copy())
+        wishlist.id++
+        logger.info("""===================================
         |= TITLE: ${wishlist.title}                   
         |= DESCRIPTION: ${wishlist.description}       
         |= ATTENDEES: ${wishlist.attendees}           
         |= COST: ${wishlist.cost}                     
         |===================================""".trimMargin())
+    }
+    else logger.info { "Not created! invalid additions" }
 }
 
 fun updateWishlist(){
+
+    var searchId = getId()
+    var wishlist = search(searchId)
+
     println("""===================================
         |=         UPDATE WISHLIST         =
         |===================================""".trimMargin())
 
-    println("title: ")
-    wishlist.title = readLine()!!
-    println("description: ")
-    wishlist.description = readLine()!!
-    println("attendees: ")
-    wishlist.attendees = readLine()!!
-    println("estimated cost: ")
-    wishlist.cost = readLine()!!
+    if(wishlist != null){
+        println("title: ")
+        wishlist.title = readLine()!!
+        println("description: ")
+        wishlist.description = readLine()!!
+        println("attendees: ")
+        wishlist.attendees = readLine()!!
+        println("estimated cost: ")
+        wishlist.cost = readLine()!!
 
-    println("""===================================
+        println("""
+        |===================================
         |= TITLE: ${wishlist.title}                   
         |= DESCRIPTION: ${wishlist.description}       
         |= ATTENDEES: ${wishlist.attendees}           
         |= COST: ${wishlist.cost}                     
         |===================================""".trimMargin())
 
+    }
 }
 
 fun deleteWishlist(){
     println("""===================================
         |=         DELETE WISHLIST         =
         |===================================""".trimMargin())
+
+    var searchId = getId()
+
+    println("wishlist ID: ")
 
 }
 
@@ -104,4 +126,24 @@ fun listWishlists(){
         |=       LIST ALL WISHLISTS        =
         |===================================""".trimMargin())
 
+    wishlists.forEach {
+        logger.info { "${it}" }
+    }
+}
+
+fun getId() : Long {
+    var strId : String? // String to hold user input
+    var searchId : Long // Long to hold converted id
+    print("Enter id to Search/Update : ")
+    strId = readLine()!!
+    searchId = if (strId.toLongOrNull() != null && !strId.isEmpty())
+        strId.toLong()
+    else
+        -9
+    return searchId
+}
+
+fun search(id: Long) : WishlistModel? {
+    var foundWishlist: WishlistModel? = wishlists.find { p -> p.id == id }
+    return foundWishlist
 }
--- src/main/kotlin/org/setu/wishlist/console/models/WishlistModel.kt
+++ src/main/kotlin/org/setu/wishlist/console/models/WishlistModel.kt
@@ -1,6 +1,7 @@
 package org.setu.wishlist.console.models
 
-data class WishlistModel(var title: String = "",
+data class WishlistModel(var id: Long = 0,
+                         var title: String = "",
                          var description: String = "",
                          var attendees: String = "",
                          var cost: String = ""){
